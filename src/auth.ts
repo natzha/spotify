c8ea9850c5a7528a33c8d60c795385d8d5432344
natzha <50129222+natzha@users.noreply.github.com>
2024-12-30 16:00:49 +1100

    bugfix for out of token

--- src/auth.ts
+++ src/auth.ts
@@ -158,7 +158,11 @@ export async function refreshAccessTokenPKCE(clientId: string, refresh_token: st
     });
 
     if (!response.ok) {
-        throw new Error(`Failed to refresh access token: ${response.statusText}`);
+        console.log(`Failed to refresh access token: ${response.statusText}`);
+        logout()
+        window.location.href = "/spotify/login/";
+        return;
+        // throw new Error(`Failed to refresh access token: ${response.statusText}`);
     }
 
     const access_token = await response.json();
